d5623c9ea379ee6546971255bdccd8d757d239e1
Shahryar <shahryar.sabouri.1301@gmail.com>
2025-02-13 12:53:47 +0100

    AuthController.php created, relevant credentials added

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request) {
+        try {
+            $request->validate([
+                'name' => 'required',
+                'email' => 'required|email|unique:users',
+                'password' => 'required|min:6',
+            ]);
+
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+
+            return response()->json(['message' => "New user successfully registered"], 201);
+        } catch (\Exception $e) {
+            return response()->json(['message' => "Registration failed"], 400);
+        }
+    }
+
+    public function login(Request $request) {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+
+        return response()->json(['token' => $user->createToken('API Token')->plainTextToken]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+        return response()->json(['message' => 'Logged out']);
+    }
+}
