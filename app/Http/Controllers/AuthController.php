22e2227d2016c0d5179e6619aea3808fdcb1e1cf
Shahryar <shahryar.sabouri.1301@gmail.com>
2025-02-13 13:17:06 +0100

    Revision in AuthController.php, logout route added in api.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
@@ -29,23 +30,29 @@ class AuthController extends Controller
     }
 
     public function login(Request $request) {
-        $request->validate([
-            'email' => 'required|email',
-            'password' => 'required',
+        try {
+            $request->validate([
+                'email' => 'required|email',
+                'password' => 'required',
         ]);
 
         $user = User::where('email', $request->email)->first();
 
         if (!$user || !Hash::check($request->password, $user->password)) {
-            return response()->json(['message' => 'Unauthorized'], 401);
+            throw ValidationException::withMessages([
+                'message' => ['Unauthorized'],
+            ]);
         }
 
-        return response()->json(['token' => $user->createToken('API Token')->plainTextToken]);
+            return response()->json(['token' => $user->createToken('API Token')->plainTextToken]);
+        } catch(\Exception $e) {
+            return response()->json(['message' => $e->getMessage()], 401);
+        }
     }
 
-    public function logout(Request $request)
-    {
+    public function logout(Request $request) {
         $request->user()->tokens()->delete();
-        return response()->json(['message' => 'Logged out']);
+
+        return response()->json(['message' => 'Logged out'], 200);
     }
 }
